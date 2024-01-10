6a3bd97598d943120878ecc70c58644a4a423eec
DmitriyVx <113830810+DmitriyVx@users.noreply.github.com>
2024-01-10 23:44:25 +0300

    Add files via upload

---
+++ Dynamic_Structures.ixx
@@ -0,0 +1,7 @@
+// Файл Dynamic_Structures.ixx
+
+export module Dynamic_Structures;
+
+export import :Abstract_Class_Stack;
+export import :Stack_Based_On_Unidirectional_Cyclic_Linked_List;
+export import :Stack_Based_On_Bidirectional_Linked_List;
---
+++ main.cpp
@@ -0,0 +1,44 @@
+// Файл main.cpp
+
+import Dynamic_Structures;
+#include <iostream>
+using namespace Dynamic_Structures;
+// Функция, демонстрирующая работу со стеками, унаследованными от базового класса
+template <typename T>
+void demonstrate(AbstractClassStack<T>& structure, const std::initializer_list<T>& elements) {
+    std::cout << "Добавление элементов в структуру: ";
+    for (const auto& element : elements) {
+        structure.Push(element);
+        std::cout << element << "  ";
+    }
+    std::cout << "\n";
+
+    std::cout << "Размер структуры: " << structure.GetSize() << "\n";
+
+    T topElement;
+    if (structure.Peek(topElement)) {
+        std::cout << "Верхний элемент: " << topElement << "\n";
+    }
+
+    std::cout << "Извлечение элементов из структуры: ";
+    while (structure.GetSize() > 0) {
+        structure.Peek(topElement);
+        std::cout << topElement << "  ";
+        structure.Pop(topElement);
+    }
+    std::cout << "\n";
+};
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    std::cout << "\n" << "Стек на основе однонаправленного списка" << "\n";
+    Dynamic_Structures::StackBasedOnBidirectionalLinkedList<int> intStack;
+    demonstrate(intStack, { 1, 2, 3, 4, 5});
+
+    std::cout << "\n" <<"Стек на основе двунаправленного циклического списка" << "\n";
+    Dynamic_Structures::StackBasedOnBidirectionalLinkedList<double> doubleStack;
+    demonstrate(doubleStack, { 1.1, 2.2, 3.3, 4.4, 5.5 });
+
+	return 0;
+}
